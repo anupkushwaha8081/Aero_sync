faa1fc32d1904b6bba7c643ee9bb257fb96c3e45
Anup Kushwaha <125217613+anupkushwaha8081@users.noreply.github.com>
2025-04-27 16:44:23 +0530

    Update Signup.jsx

--- src/common/Signup.jsx
+++ src/common/Signup.jsx
@@ -265,7 +265,7 @@ const Signup = ({ setIsLoggedIn }) => {
   
     try {
       const { confirmPassword, ...payload } = formData;
-      const res = await axios.post('http://localhost:5000/api/auth/register', payload);
+      const res = await axios.post('https://anup-airline.onrender.com/api/auth/register', payload);
       
       if (res.data && res.data.success) {
         toast.success('Account created successfully!');
